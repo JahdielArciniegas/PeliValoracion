6491b5fa58ac932ab080c7da79411ea2162cf97d
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2026-01-02 19:31:16 -0400

    feat: add rating functionality to coupleMovie.service

--- src/services/coupleMovie.service.ts
+++ src/services/coupleMovie.service.ts
@@ -23,3 +23,50 @@ const markMovieWatched = async (movie: CoupleMovie) => {
   const result = await coupleMoviesRepository.markMovieWatched(movie);
   return result;
 };
+
+const ratingMovie = async (
+  userId: string,
+  coupleId: string,
+  movieId: string,
+  rating: number,
+  opinion: string
+) => {
+  const movie = await coupleMoviesRepository.getOneMovie(coupleId, movieId);
+  if (!movie) {
+    throw new Error("Movie not found");
+  }
+
+  if (movie.ratings.some((rating) => rating.userId === userId)) {
+    throw new Error("User already rated this movie");
+  }
+
+  if (!userId || !coupleId || !movieId || !rating || !opinion) {
+    throw new Error("Invalid rating data");
+  }
+
+  if (rating < 1 || rating > 10) {
+    throw new Error("Rating must be between 1 and 10");
+  }
+
+  if (movie.ratings.length < 2) {
+    throw new Error("Movie must have at least 2 ratings");
+  }
+
+  if (movie.ratings.length === 0) {
+    movie.ratings = [];
+  }
+
+  const newRating = {
+    userId,
+    rating,
+    opinion,
+  };
+
+  const newMovie = {
+    ...movie,
+    ratings: [...movie.ratings, newRating],
+  };
+
+  const movieResult = await coupleMoviesRepository.updateMovie(newMovie);
+  return movieResult;
+};
