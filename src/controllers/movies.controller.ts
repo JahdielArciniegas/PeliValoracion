bf0e1b4ee7db1a55d37ab0684d7567f60bd54817
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2026-01-06 09:10:36 -0400

    feat: migrate search term extraction from req.query to req.params in movies.controller

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from "express";
 import { moviesService } from "../services/movies.service.js";
 
 const searchMovies = async (req: Request, res: Response) => {
-  const query = req.query.query as string;
+  const query = req.params.query as string;
   const movies = await moviesService.searchMovies(query);
   res.status(200).json(movies);
 };
