7d186a0385e2544da7317ef1048da91aedd10b49
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2026-01-06 08:05:54 -0400

    feat: implement nested routing with multiple parameters in coupleMovie.routes

--- src/routes/coupleMovie.routes.ts
+++ src/routes/coupleMovie.routes.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { coupleMoviesController } from "../controllers/coupleMovies.controller.js";
 
-const coupleMovieRouter = express.Router();
+const coupleMovieRouter = express.Router({ mergeParams: true });
 
 coupleMovieRouter.post("/", coupleMoviesController.markMovieWatched);
-coupleMovieRouter.get("/:id", coupleMoviesController.getMovieWatched);
-coupleMovieRouter.get("/", coupleMoviesController.getAllMoviesWatched);
-coupleMovieRouter.post("/rate/:movieId", coupleMoviesController.rateMovie);
+coupleMovieRouter.get("/:idcouple/:id", coupleMoviesController.getMovieWatched);
+coupleMovieRouter.get("/:idcouple", coupleMoviesController.getAllMoviesWatched);
+coupleMovieRouter.post(
+  "/:idcouple/rate/:movieId",
+  coupleMoviesController.rateMovie
+);
 
 export default coupleMovieRouter;
