cd0c4c2aa93745a05883c8e98b89445893b5876a
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2026-01-07 08:35:55 -0400

    refactor: use centralized errorHandler for user.service exceptions

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,13 @@
 import type { User } from "../interfaces/user.js";
 import { userRepositories } from "../repositories/user.repositories.js";
+import { ValidationError, NotFoundError } from "../utils/errorHandler.js";
 
 const create = async (name: string, email: string) => {
-  if (!name || !email) throw new Error("User name and email are required");
+  if (!name || !email)
+    throw new ValidationError("User name and email are required");
   const coupleId = "";
   const userExist = await userRepositories.getOneByEmail(email);
-  if (userExist) throw new Error("User already exists");
+  if (userExist) throw new ValidationError("User already exists");
   const newUser: User = {
     name: name,
     email: email,
@@ -16,17 +18,17 @@ const create = async (name: string, email: string) => {
 };
 
 const getOne = async (email: string) => {
-  if (!email) throw new Error("Email is required");
+  if (!email) throw new ValidationError("Email is required");
   const user = await userRepositories.getOneByEmail(email);
-  if (!user) throw new Error("User not found");
+  if (!user) throw new NotFoundError("User not found");
   return user;
 };
 
 const update = async (id: string, user: User) => {
-  if (!user.email) throw new Error("Email is required");
+  if (!user.email) throw new ValidationError("Email is required");
   const userExist = await userRepositories.getOneById(id);
-  if (!userExist) throw new Error("User not found");
-  if (!user.name) throw new Error("User name is required");
+  if (!userExist) throw new NotFoundError("User not found");
+  if (!user.name) throw new ValidationError("User name is required");
   const coupleId = user.coupleId || userExist.coupleId;
   const newUser: User = {
     name: user.name,
@@ -38,9 +40,9 @@ const update = async (id: string, user: User) => {
 };
 
 const remove = async (id: string) => {
-  if (!id) throw new Error("Id is required");
+  if (!id) throw new ValidationError("Id is required");
   const userExist = await userRepositories.getOneById(id);
-  if (!userExist) throw new Error("User not found");
+  if (!userExist) throw new NotFoundError("User not found");
   const userRemove = await userRepositories.remove(id);
   return userRemove;
 };
