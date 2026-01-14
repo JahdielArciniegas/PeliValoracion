17269872579fd5d59c05b3e2fcc68f4a835693d0
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2026-01-14 15:39:11 -0400

    feat: add new endpoints to movies routes

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -4,5 +4,8 @@ import { moviesController } from "../controllers/movies.controller.js";
 const movieRouter = express.Router({ mergeParams: true });
 
 movieRouter.get("/search/:query", moviesController.searchMovies);
+movieRouter.get("/nowplaying", moviesController.nowplayingMovies);
+movieRouter.get("/toprated", moviesController.topRatedMovies);
+movieRouter.get("/popular", moviesController.popularMovies);
 
 export default movieRouter;
