717abe2268a3b382cd0338e2a3982193e6de4a96
Jahdiel Arciniegas <arciniegasjahdiel@gmail.com>
2026-04-30 19:10:29 -0400

    style: format user.repositories.ts with prettier

--- src/modules/user/user.repositories.ts
+++ src/modules/user/user.repositories.ts
@@ -1,5 +1,5 @@
-import type { User as UserInterface, UserUpdate } from "./user.js";
-import User from "./user.model.js";
+import type { User as UserInterface, UserUpdate } from './user.js'
+import User from './user.model.js'
 
 const create = async (user: UserInterface) => {
   const newUser = new User({
@@ -7,32 +7,32 @@ const create = async (user: UserInterface) => {
     email: user.email,
     coupleId: user.coupleId,
     password: user.password,
-  });
-  const result = await newUser.save();
-  return result;
-};
+  })
+  const result = await newUser.save()
+  return result
+}
 
 const getOneByEmail = async (email: string) => {
-  const result = await User.findOne<UserInterface>({ email: email });
-  return result;
-};
+  const result = await User.findOne<UserInterface>({ email: email })
+  return result
+}
 
 const getOneById = async (id: string) => {
-  const result = await User.findOne<UserInterface>({ _id: id });
-  return result;
-};
+  const result = await User.findOne<UserInterface>({ _id: id })
+  return result
+}
 
 const update = async (user: UserUpdate) => {
   const result = await User.findByIdAndUpdate(user.id, user, {
     new: true,
-  }).populate("coupleId");
-  return result;
-};
+  }).populate('coupleId')
+  return result
+}
 
 const remove = async (id: string) => {
-  const result = await User.deleteOne({ _id: id });
-  return result;
-};
+  const result = await User.deleteOne({ _id: id })
+  return result
+}
 
 export const userRepositories = {
   create,
@@ -40,4 +40,4 @@ export const userRepositories = {
   getOneById,
   update,
   remove,
-};
+}
