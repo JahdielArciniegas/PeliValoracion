8e9c9cd09209cd67c1ac513e3f600c76889c16c5
Jahdiel Arciniegas <arciniegasjahdiel@gmail.com>
2026-06-05 16:59:01 -0400

    test: update test suite to match new authentication logic in coupleMovie

--- test/coupleMovie.test.ts
+++ test/coupleMovie.test.ts
@@ -1,13 +1,14 @@
 import supertest from 'supertest'
 import app from '../src/app.js'
 import mongoose from 'mongoose'
-import { describe, test, beforeAll, afterAll, expect } from 'vitest'
+import { describe, test, beforeAll, afterAll, expect, afterEach } from 'vitest'
 import { authService } from '../src/modules/auth/auth.service.js'
 import User from '../src/modules/user/user.model.js'
 import Couple from '../src/modules/couple/couple.model.js'
 import type { Couple as CoupleInterface } from '../src/modules/couple/couple.js'
 import CoupleMovie from '../src/modules/coupleMovie/coupleMovie.model.js'
 import { coupleRepositories } from '../src/modules/couple/couple.repositories.js'
+import { userRepositories } from '../src/modules/user/user.repositories.js'
 
 const api = supertest(app)
 
@@ -50,8 +51,6 @@ const badMovieTest = {
 let cookie: string
 let coupleId: string
 let id: string
-let id2: string
-let id3: string
 
 beforeAll(async () => {
   if (mongoose.connection.readyState === 0) {
@@ -75,15 +74,13 @@ beforeAll(async () => {
     userTest2.password
   )
 
-  const user3 = await authService.register(
+  await authService.register(
     userTest3.name,
     userTest3.email,
     userTest3.password
   )
 
   id = user1.id.toString()
-  id2 = user2.id.toString()
-  id3 = user3.id.toString()
 
   const coupleTest = {
     users: [user1.id, user2.id],
@@ -99,12 +96,31 @@ beforeAll(async () => {
     coupleTest as unknown as CoupleInterface
   )
 
-  const loginResponse = await api.post('/api/auth/login').send(userTest)
-  cookie = loginResponse.headers['set-cookie']
+  const userCouple1 = {
+    id: user1.id.toString(),
+    name: user1.name,
+    email: user1.email,
+    password: user1.password,
+    coupleId: coupleId,
+  }
+
+  const userCouple2 = {
+    id: user2.id.toString(),
+    name: user2.name,
+    email: user2.email,
+    password: user2.password,
+    coupleId: coupleId,
+  }
+
+  await userRepositories.update(userCouple1)
+  await userRepositories.update(userCouple2)
 })
 
 describe('mark movie watched', () => {
   test('marca una pelicula vista con un id de pareja invalido', async () => {
+    const loginResponse = await api.post('/api/auth/login').send(userTest)
+    cookie = loginResponse.headers['set-cookie']
+
     const response = await api
       .post(`/api/coupleMovie/12345566`)
       .set('Cookie', cookie)
@@ -113,16 +129,20 @@ describe('mark movie watched', () => {
     expect(response.status).toBe(400)
   })
 
-  test('marca una pelicula vista con una pareja que no existe', async () => {
+  test('marca una pelicula vista con una pareja que no es la del usuario', async () => {
+    const loginResponse = await api.post('/api/auth/login').send(userTest)
+    cookie = loginResponse.headers['set-cookie']
     const response = await api
       .post(`/api/coupleMovie/${id}`)
       .set('Cookie', cookie)
       .send(movieTest)
 
-    expect(response.status).toBe(404)
+    expect(response.status).toBe(403)
   })
 
   test('marca una pelicula vista con un dato faltante', async () => {
+    const loginResponse = await api.post('/api/auth/login').send(userTest)
+    cookie = loginResponse.headers['set-cookie']
     const response = await api
       .post(`/api/coupleMovie/${coupleId}`)
       .set('Cookie', cookie)
@@ -132,15 +152,20 @@ describe('mark movie watched', () => {
   })
 
   test('marca una pelicula vista con datos Validos', async () => {
+    const loginResponse = await api.post('/api/auth/login').send(userTest)
+    cookie = loginResponse.headers['set-cookie']
+
     const response = await api
       .post(`/api/coupleMovie/${coupleId}`)
       .set('Cookie', cookie)
       .send(movieTest)
 
-    expect(response.status).toBe(200)
+    expect(response.status).toBe(201)
   })
 
   test('marca una pelicula vista con una pelicula que ya esta vista', async () => {
+    const loginResponse = await api.post('/api/auth/login').send(userTest)
+    cookie = loginResponse.headers['set-cookie']
     const response = await api
       .post(`/api/coupleMovie/${coupleId}`)
       .set('Cookie', cookie)
@@ -152,6 +177,8 @@ describe('mark movie watched', () => {
 
 describe('Rating Couple Movie', () => {
   test('Rankear una pelicula sin estar marcada como vista', async () => {
+    const loginResponse = await api.post('/api/auth/login').send(userTest2)
+    cookie = loginResponse.headers['set-cookie']
     const response = await api
       .post(`/api/coupleMovie/${coupleId}/movies/${movieTest2.movieId}/rating`)
       .set('Cookie', cookie)
@@ -159,13 +186,14 @@ describe('Rating Couple Movie', () => {
         rating: 10,
         opinion:
           'muy buena la verdad esta increible, me gusto mucho es muy buena',
-        userId: id2,
       })
 
     expect(response.status).toBe(404)
   })
 
   test('Rankear una pelicula ya rankeada por el usuario', async () => {
+    const loginResponse = await api.post('/api/auth/login').send(userTest2)
+    cookie = loginResponse.headers['set-cookie']
     const response = await api
       .post(`/api/coupleMovie/${coupleId}/movies/${movieTest.movieId}/rating`)
       .set('Cookie', cookie)
@@ -173,7 +201,6 @@ describe('Rating Couple Movie', () => {
         rating: 10,
         opinion:
           'muy buena la verdad esta increible, me gusto mucho es muy buena',
-        userId: id2,
       })
 
     expect(response.status).toBe(200)
@@ -185,22 +212,25 @@ describe('Rating Couple Movie', () => {
         rating: 10,
         opinion:
           'muy buena la verdad esta increible, me gusto mucho es muy buena',
-        userId: id2,
       })
 
     expect(response2.status).toBe(400)
   })
 
   test('Rankear una pelicula con un string invalido', async () => {
+    const loginResponse = await api.post('/api/auth/login').send(userTest)
+    cookie = loginResponse.headers['set-cookie']
     const response = await api
       .post(`/api/coupleMovie/${coupleId}/movies/${movieTest.movieId}/rating`)
       .set('Cookie', cookie)
-      .send({ rating: 10, userId: id })
+      .send({ rating: 10 })
 
     expect(response.status).toBe(400)
   })
 
-  test('Rankear una pelicula con un rating fura de 1-10', async () => {
+  test('Rankear una pelicula con un rating fuera de 1-10', async () => {
+    const loginResponse = await api.post('/api/auth/login').send(userTest)
+    cookie = loginResponse.headers['set-cookie']
     const response = await api
       .post(`/api/coupleMovie/${coupleId}/movies/${movieTest.movieId}/rating`)
       .set('Cookie', cookie)
@@ -208,13 +238,14 @@ describe('Rating Couple Movie', () => {
         rating: 11,
         opinion:
           'muy buena la verdad esta increible, me gusto mucho es muy buena',
-        userId: id,
       })
 
     expect(response.status).toBe(400)
   })
 
   test('Rankear una pelicula con mas de 2 personas', async () => {
+    const loginResponse = await api.post('/api/auth/login').send(userTest)
+    cookie = loginResponse.headers['set-cookie']
     await api
       .post(`/api/coupleMovie/${coupleId}`)
       .set('Cookie', cookie)
@@ -227,11 +258,14 @@ describe('Rating Couple Movie', () => {
         rating: 10,
         opinion:
           'muy buena la verdad esta increible, me gusto mucho es muy buena',
-        userId: id,
       })
 
     expect(response.status).toBe(200)
 
+    await api.post('/api/auth/logout')
+
+    const loginResponse2 = await api.post('/api/auth/login').send(userTest2)
+    cookie = loginResponse2.headers['set-cookie']
     const response2 = await api
       .post(`/api/coupleMovie/${coupleId}/movies/${movieTest2.movieId}/rating`)
       .set('Cookie', cookie)
@@ -239,11 +273,14 @@ describe('Rating Couple Movie', () => {
         rating: 10,
         opinion:
           'muy buena la verdad esta increible, me gusto mucho es muy buena',
-        userId: id2,
       })
 
     expect(response2.status).toBe(200)
 
+    await api.post('/api/auth/logout')
+
+    const loginResponse3 = await api.post('/api/auth/login').send(userTest3)
+    cookie = loginResponse3.headers['set-cookie']
     const response3 = await api
       .post(`/api/coupleMovie/${coupleId}/movies/${movieTest2.movieId}/rating`)
       .set('Cookie', cookie)
@@ -251,13 +288,14 @@ describe('Rating Couple Movie', () => {
         rating: 10,
         opinion:
           'muy buena la verdad esta increible, me gusto mucho es muy buena',
-        userId: id3,
       })
 
-    expect(response3.status).toBe(400)
+    expect(response3.status).toBe(403)
   })
 
   test('Rankear una pelicula correctamente', async () => {
+    const loginResponse = await api.post('/api/auth/login').send(userTest)
+    cookie = loginResponse.headers['set-cookie']
     const response = await api
       .post(`/api/coupleMovie/${coupleId}/movies/${movieTest.movieId}/rating`)
       .set('Cookie', cookie)
@@ -265,11 +303,14 @@ describe('Rating Couple Movie', () => {
         rating: 10,
         opinion:
           'muy buena la verdad esta increible, me gusto mucho es muy buena',
-        userId: id,
       })
 
     expect(response.status).toBe(200)
   })
+})
+
+afterEach(async () => {
+  await api.post('/api/auth/logout')
 })
 
 afterAll(async () => {
